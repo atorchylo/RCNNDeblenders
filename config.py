84b38d8c4715d717104d36f893c66b857e0eb07f
Andrii Torchylo <andrii.torchylo@gmail.com>
2022-08-16 17:52:02 -0700

    config refactored

--- config.py
+++ config.py
@@ -3,94 +3,95 @@
 import json
 
 
-
 class BaseConfig:  
     
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
-    
+
+    def _to_dict(self):
+        values = [name for name in dir(self) if not name.startswith('_')]
+        return {name: self.__getattribute__(name) for name in values}
+
     def __repr__(self):
-        values = [name for name in dir(self) if not name.startswith('__')]
-        params = {name: self.__getattribute__(name) for name in values}
-        print_params = json.dumps(params, indent=4)
+        # TODO Better representation of config (?)
+        print_params = json.dumps(self._to_dict(), indent=4)
         string = f'{type(self).__name__}:\n{print_params}'
         return string
 
-class ImagesConfig(BaseConfig):
-    in_channels: int = 6
-    num_classes: int = 2
-    img_resolution: int = (128, 128)
 
-class BoxConfig(BaseConfig):
-    anchor_sizes = ((4,), (8,), (16,), (32,), (64,))
-    aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
+class ImagesConfig(BaseConfig):
+    in_channels = 6
+    num_classes = 2  # galaxy + background
+    img_resolution = (128, 128)
+
 
 class BackboneConfig(BaseConfig):
-    backbone_name: str = "resnet50"
-    num_feature_maps: int = 5
-    num_out_channels: int = 256
+    name = "resnet50"
+    input_channels = 6
 
-# TODO FIX
+
 class RPNConfig(BaseConfig):
-    rpn_pre_nms_top_n_train: int = 2000
-    rpn_pre_nms_top_n_test: int = 1000
-    rpn_post_nms_top_n_train: int = 2000
-    rpn_post_nms_top_n_test: int = 1000
-    rpn_nms_thresh: float = 0.7
-    rpn_fg_iou_thresh: float = 0.7
-    rpn_bg_iou_thresh: float = 0.3
-    rpn_batch_size_per_image: int = 256
-    rpn_positive_fraction: float = 0.5
-    rpn_score_thresh: float = 0.0
+    anchor_sizes = ((4,), (8,), (16,), (32,), (64,))
+    aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
+    pre_nms_top_n_train = 2000
+    pre_nms_top_n_test = 1000
+    post_nms_top_n_train = 2000
+    post_nms_top_n_test = 1000
+    nms_thresh = 0.7
+    fg_iou_thresh = 0.7
+    bg_iou_thresh = 0.3
+    batch_size_per_image = 128
+    positive_fraction = 0.5
+    score_thresh = 0.0
 
-# TODO FIX
-class BoxConfig(BaseConfig):
-    # Box parameters
-    box_score_thresh: float = 0.05
-    box_nms_thresh: float = 0.5
-    box_detections_per_img: int = 100
-    box_fg_iou_thresh: float = 0.5
-    box_bg_iou_thresh: float = 0.5
-    box_batch_size_per_image: int = 512
-    box_positive_fraction: float = 0.25
-    bbox_reg_weights: Optional[Tuple[float, float, float, float]] = None
+
+class ROIBoxHeadConfig(BaseConfig):
+    output_pool_size = 7
+    score_thresh = 0.05
+    nms_thresh = 0.5
+    detections_per_img = 100
+    fg_iou_thresh = 0.5
+    bg_iou_thresh = 0.5
+    batch_size_per_image = 512
+    positive_fraction = 0.25
+    bbox_reg_weights = None
 
 # TODO FIX
 class EllipseConfig(BaseConfig):
     pass
 
-# TODO FIX
-class MetaConfig():
-    def __init__(
-        self, 
-        images = ImagesConfig, 
-        backbone = BackboneConfig, 
-        rpn = RPNConfig, 
-        box = BoxConfig, 
-        ellipse = EllipseConfig
-    ):
-        self.images = images()
-        self.backbone = backbone()
-        self.rpn = rpn()
-        self.ellipse = ellipse()
-    
-    def __repr__(self):
-        return f'{self.images}\n\n{self.backbone}\n\n{self.rpn}\n\n{self.ellipse}'
-    
-    def to_json(self, path):
-        
-        dictionary  = {key: self.__dict__[key].__dic for key in self.__dict__}
-        json_object = json.dumps(, indent=4)
-        with open(path, "w") as f:
-            f.write(json_object)
-        
-    def from_json(self):
-        pass
-    # @classmethod
-    # def from_dict(cls, the_dict) -> PlotConfig:
-    #     """Create a PlotConfig object from a dict"""
-    #     return cls(
-    #         plotname=the_dict["plotname"],
-    #         classname=the_dict["classname"],
-    #         plot_config=the_dict["plot_config"],
-    #     )
+# # TODO FIX
+# class MetaConfig():
+#     def __init__(
+#         self,
+#         images = ImagesConfig,
+#         backbone = BackboneConfig,
+#         rpn = RPNConfig,
+#         box = BoxConfig,
+#         ellipse = EllipseConfig
+#     ):
+#         self.images = images()
+#         self.backbone = backbone()
+#         self.rpn = rpn()
+#         self.ellipse = ellipse()
+#
+#     def __repr__(self):
+#         return f'{self.images}\n\n{self.backbone}\n\n{self.rpn}\n\n{self.ellipse}'
+#
+#     def to_json(self, path):
+#
+#         dictionary  = {key: self.__dict__[key].__dic for key in self.__dict__}
+#         #json_object = json.dumps(, indent=4)
+#         with open(path, "w") as f:
+#             f.write(json_object)
+#
+#     def from_json(self):
+#         pass
+#     # @classmethod
+#     # def from_dict(cls, the_dict) -> PlotConfig:
+#     #     """Create a PlotConfig object from a dict"""
+#     #     return cls(
+#     #         plotname=the_dict["plotname"],
+#     #         classname=the_dict["classname"],
+#     #         plot_config=the_dict["plot_config"],
+#     #     )
